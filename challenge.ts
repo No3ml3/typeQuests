af7acecfa3fc657daea9a3e7962139cc4aace351
Noemie.BARRE <noemie.barre24@gmail.com>
2023-11-06 09:55:05 +0100

    a

--- challenge.ts
+++ challenge.ts
@@ -3,16 +3,16 @@ interface User {
     age: number
   }
 
-const prettyPrintWilder = (users: User[]) => {
+const prettyPrintWilder = (users: User[]):void => {
     users.map((user) => {
       console.log(`${user.name} is ${user.age} years old`);
     });
   };
   
-  const wilders = [];
-  const user1 = { name: "Pierre", age: 23 };
-  const user2 = { name: "Paul", age: 23 };
-  const user3 = { name: "Jacques", age: 25 };
+  const wilders:User[] = [];
+  const user1:User = { name: "Pierre", age: 23 };
+  const user2:User = { name: "Paul", age: 23 };
+  const user3:User = { name: "Jacques", age: 25 };
   wilders.push(user1);
   wilders.push(user2);
   wilders.push(user3);
